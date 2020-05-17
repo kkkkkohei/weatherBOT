0a2d80d51a9c6081cd1376eef1f96648e3ef881d
kohei morioka <spade.mk21@icloud.com>
2020-05-18 04:00:55 +0900

    Initial Commit

---
+++ weatherBOT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherBOT
+//
+//  Created by kohei morioka on 2020/05/18.
+//  Copyright © 2020 kohei morioka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
