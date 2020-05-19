371cbe6ed8fb55ec6e02f9bb3fc134d837fa2d4c
kohei morioka <spade.mk21@icloud.com>
2020-05-19 19:04:18 +0900

    緯度経度が取得できません。

--- weatherBOT/ViewController.swift
+++ weatherBOT/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 import CoreLocation
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController {
 
     @IBOutlet weak var datePicker: UIDatePicker!
     
+    // 通知時間
     var callTime = ""
-    var latlng = ""
+    // 緯度
+    var latitudeNow: String = ""
+    // 経度
+    var longitudeNow: String = ""
+    
+    // ロケーションマネージャ
     var locationManager: CLLocationManager!
     
     override func viewDidLoad() {
@@ -37,6 +43,15 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
         // ロケーションマネージャのセットアップ
         setupLocationManager()
+        
+        // 位置情報を記録
+        // マネージャの設定
+        let status = CLLocationManager.authorizationStatus()
+        if status == .denied {
+            showAlert()
+        } else if status == .authorizedWhenInUse {
+            print("\(latitudeNow), \(longitudeNow)")
+        }
         
     }
     
@@ -61,6 +76,23 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         }
     }
     
+    // アラートを表示する
+    func showAlert() {
+        let alertTitle = "位置情報取得が許可されていません。"
+        let alertMessage = "設定アプリの「プライバシー > 位置情報サービス」から変更してください。"
+        let alert: UIAlertController = UIAlertController(
+            title: alertTitle, message: alertMessage, preferredStyle: UIAlertController.Style.alert
+        )
+        // okボタン
+        let defaultAction: UIAlertAction = UIAlertAction(
+            title: "OK", style: UIAlertAction.Style.default, handler: nil
+        )
+        // UIAlertControllerにActionを追加
+        alert.addAction(defaultAction)
+        // Alertを表示
+        present(alert, animated: true, completion: nil)
+    }
+    
     @objc internal func onDidChangeDate(sender: UIDatePicker){
         
         // フォーマットを生成.
@@ -72,11 +104,24 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         callTime = mySelectedDate as String
         print(callTime)
     }
+
+
+}
+
+extension ViewController: CLLocationManagerDelegate {
     
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    /// 位置情報が更新された際、位置情報を格納する
+    /// - Parameters:
+    ///   - manager: ロケーションマネージャ
+    ///   - locations: 位置情報
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+        let location = locations.first
+        let latitude = location?.coordinate.latitude
+        let longitude = location?.coordinate.longitude
+        // 位置情報を格納する
+        self.latitudeNow = String(latitude!)
+        self.longitudeNow = String(longitude!)
         
     }
-
-
 }
 
